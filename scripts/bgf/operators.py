87be7686298f466c57c023409ea72e407d18127d
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-05-01 01:42:17 -0300

    feat: run specific operators only if triggered by manager - print warning messages otherwise - remove trailing whitespaces

--- scripts/bgf/operators.py
+++ scripts/bgf/operators.py
@@ -1,47 +1,48 @@
 import bge
 from bge.types import *
+from . manager import Manager
 
 
 # Default operators
 def applyConfig(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import config
-    
+
     # Resolution
     if config["Resolution"] in bge.logic.__resolutions.keys():
         resolution = bge.logic.__resolutions[config["Resolution"]]
         bge.render.setWindowSize(resolution[0], resolution[1])
-        
+
     # Fullscreen
     bge.render.setFullScreen(bool(config["Fullscreen"]))
-    
+
     # Anisotropic filtering
     if config["AnisotropicFiltering"] in (1, 2, 4, 8, 16):
         bge.render.setAnisotropicFiltering(config["AnisotropicFiltering"])
-    
+
     # Mipmapping
     mipmapLevels = {
         "None" : 0,
         "Nearest" : 1,
         "Linear" : 2,
     }
-    
+
     if config["Mipmaps"] in mipmapLevels.keys():
         bge.render.setMipmapping(mipmapLevels[config["Mipmaps"]])
-        
+
     # Motion blur
     motionBlur = round(config["MotionBlur"], 1)
-    
+
     if motionBlur == 0:
         bge.render.disableMotionBlur()
-        
+
     elif 0 < motionBlur <= 1:
         bge.render.enableMotionBlur(motionBlur)
-        
+
     # Vsync
     bge.render.setVsync(bool(config["Vsync"]))
-    
+
     # GLSL material settings
     glslSettings = {
         "lights" : config["Lights"],
@@ -51,42 +52,48 @@ def applyConfig(cont, arg=""):
         "nodes" : config["Nodes"],
         "extra_textures" : config["ExtraTextures"],
     }
-    
+
     for option in glslSettings.keys():
         bge.render.setGLSLMaterialSetting(option, bool(glslSettings[option]))
 
 
 def exitGame(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
-    own = cont.owner
-    own["ContextState"] = "ExitGame"
-    own["ContextTransition"] = True
+
+    own = cont.owner # type: KX_GameObject
+
+    if type(own) == Manager:
+        own = own # type: Manager
+        own.contextState = "ExitGame"
+        own.contextTransition = True
+
+    else:
+        print("X Exit game must be called from manager")
 
 
 def hideMouseCursor(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     showMouseCursor(cont, "False")
 
 
 def loadState(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import curPath, state, loadFile
-    
+
     if arg:
         targetFile = curPath / ("save/" + arg + ".dat")
-            
+
         if targetFile.exists():
             state.update(loadFile(targetFile))
             print("> Loaded state from save/" + arg + ".dat")
         else:
             print("X Could not load save/" + arg + ".dat")
-        
+
     else:
         targetFile = curPath / "save.dat"
-        
+
         if targetFile.exists():
             state.update(loadFile(targetFile))
             print("> Loaded state from save.dat")
@@ -96,44 +103,57 @@ def loadState(cont, arg=""):
 
 def pauseContext(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import database
     own = cont.owner
-    
-    if own["Context"] in database["Contexts"].keys():
-        ctxScns = database["Contexts"][own["Context"]]["Scenes"] # type: list[dict]
-        sceneList = {} # type: dict[KX_Scene]
-        
-        for scn in bge.logic.getSceneList():
-            if scn.name != "ScnManager":
-                sceneList[scn.name] = scn
-        
-        for scn in ctxScns:
-            if scn.get("Name") in sceneList.keys() and scn.get("Pausable"):
-                curScene = sceneList[scn["Name"]] # type: KX_Scene
-                
-                if (not arg or arg.lower() == "true") and not curScene.suspended:
-                    curScene.suspend()
-                
-                elif arg.lower() == "false" and curScene.suspended:
-                    curScene.resume()
+
+    if type(own) == Manager:
+        own = own # type: Manager
+
+        if own.context in database["Contexts"].keys():
+            ctxScns = database["Contexts"][own.context]["Scenes"] # type: list[dict]
+            sceneList = {} # type: dict[KX_Scene]
+
+            for scn in bge.logic.getSceneList():
+                if scn.name != "ScnManager":
+                    sceneList[scn.name] = scn
+
+            for scn in ctxScns:
+                if scn.get("Name") in sceneList.keys() and scn.get("Pausable"):
+                    curScene = sceneList[scn["Name"]] # type: KX_Scene
+
+                    if (not arg or arg.lower() == "true") and not curScene.suspended:
+                        curScene.suspend()
+
+                    elif arg.lower() == "false" and curScene.suspended:
+                        curScene.resume()
+    else:
+        print("X Pause context pause must be called from manager")
 
 
 def playBgm(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
-    if cont.owner["Bgm"] != arg:
-        cont.owner["Bgm"] = arg
-        cont.owner["BgmState"] = "FadeOut"
-        cont.owner["BgmTransition"] = True
+
+    own = cont.owner
+
+    if type(cont.owner) == Manager:
+        own = own # type: Manager
+
+        if own.bgm != arg:
+            own.bgm = arg
+            own.bgmState = "FadeOut"
+            own.bgmTransition = True
+
+    else:
+        print("X Play bgm must be called from manager")
 
 
 def playSfx(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     import aud
     from . import config, sounds
-    
+
     if config["SfxEnable"] and arg in sounds["Sfx"].keys():
         factory = aud.Factory.file(sounds["Sfx"][arg])
         handle = aud.device().play(factory) # type: aud.Handle
@@ -142,33 +162,33 @@ def playSfx(cont, arg=""):
 
 def resumeContext(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     pauseContext(cont, "False")
 
 
 def saveConfig(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import curPath, config, saveFile
     saveFile(curPath / "Config.json", config)
 
 
 def saveState(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import curPath, state, saveFile
-    
+
     if arg:
         saveDir = curPath / "save"
-        
+
         if not saveDir.exists():
             saveDir.mkdir()
-            
+
         if saveDir.exists():
             targetFile = saveDir / arg
             saveFile(targetFile, state, ".dat")
             print("> Saved state to save/" + arg + ".dat")
-        
+
     else:
         targetFile = curPath / "save.dat"
         saveFile(targetFile, state, ".dat")
@@ -177,27 +197,33 @@ def saveState(cont, arg=""):
 
 def setContext(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     from . import database
     own = cont.owner
-    
-    if arg in database["Contexts"].keys():
-        own["Context"] = arg
-        own["ContextTransition"] = True
+
+    if type(own) == Manager:
+        own = own # type: Manager
+
+        if arg in database["Contexts"].keys():
+            own.context = arg
+            own.contextTransition = True
+
+    else:
+        print("X Set context must be called from manager")
 
 
 def showMouseCursor(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     if (not arg or  str(arg).lower() == "true"):
         bge.logic.__showMouseCursor = True
-    
+
     elif str(arg).lower() == "false":
         bge.logic.__showMouseCursor = False
 
 
 def stopBgm(cont, arg=""):
     # type: (SCA_PythonController, str) -> None
-    
+
     playBgm(cont)
 
